e2b505733f95e48b80062608750b9eac78622a37
ajang <ajangirhas@gmaill.com>
2025-05-05 11:52:17 +0700

    update model Chats

--- sahate-backend/src/main/java/com/example/demo/model/Chats.java
+++ sahate-backend/src/main/java/com/example/demo/model/Chats.java
@@ -2,6 +2,11 @@ package com.example.demo.model;
 
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
@@ -13,8 +18,9 @@ public class Chats {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long Sender_id;
-    private Long receiver_id ;
+    private Long senderId;
+    private Long receiverId ;
     private String pesan;
-    private Localdate send_time;
+    @Column(name = "send_date_time")
+    private LocalDateTime sendDateTime;
 }
