782550838f0b326d59f90d0b82f1f047104e48c1
HanifFauzan-n <haniffauzann55@gmail.com>
2025-05-15 01:29:17 +0700

    Hanif Task Create Controller

---
+++ sahate-backend/src/main/java/com/example/demo/controllers/ColorController.java
@@ -0,0 +1,73 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.ColorReqDto;
+import com.example.demo.services.ColorService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/color")
+@Tag(name = "Color")
+public class ColorController {
+
+    @Autowired
+    private ColorService colorService;
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody ColorReqDto dto) {
+        try {
+            return ResponseEntity.ok(colorService.create(dto));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody ColorReqDto dto) {
+        try {
+            return ResponseEntity.ok(colorService.update(id, dto));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        try {
+            colorService.delete(id);
+            return ResponseEntity.ok().body("Color deleted");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> findById(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(colorService.findById(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {
+        try {
+            return ResponseEntity.ok(colorService.findAll());
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+}
