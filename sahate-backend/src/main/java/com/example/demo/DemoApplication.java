884151dbc71fa8d4905123c71a529e46e58b83ae
GalaxyBuilder-Oss <galaxybuilder.oss@gmail.com>
2025-06-03 13:19:55 +0700

    Add jwtFilter, jwtutils, and securityConfig

--- sahate-backend/src/main/java/com/example/demo/DemoApplication.java
+++ sahate-backend/src/main/java/com/example/demo/DemoApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SahateApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SahateApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
